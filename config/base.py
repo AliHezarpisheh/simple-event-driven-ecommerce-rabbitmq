357e3fe93d178ad0391490b9bc454448dc22a429
ali <alihezarpisheh@outlook.com>
2024-12-13 19:44:23 +0330

    ✨ Feature(config): Introduce `Settings`

--- config/base.py
+++ config/base.py
@@ -1,6 +1,10 @@
 """Module for defining base configurations."""
 
 from .logging import LoggingConfig
+from .settings import Settings
 
 # Logging
 logger = LoggingConfig().get_logger()
+
+# Settings
+settings = Settings()  # type: ignore
