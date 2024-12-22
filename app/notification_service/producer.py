7b188e018767ac42d03d8abaa40987518bfbe062
alihezarpishe <alihezarpisheh@outlook.com>
2024-12-22 16:11:53 +0330

    ✨ Feature: Implement `notification_service` components

---
+++ app/notification_service/producer.py
@@ -0,0 +1,80 @@
+"""Module handles the publishing of notification messages to a RabbitMQ exchange."""
+
+import asyncio
+import random
+from typing import get_args
+
+from aio_pika import DeliveryMode, Message
+
+from app.notification_service.pubsub import NotificationPubSub
+from app.notification_service.schemas import Notification, NotificationType
+from app.payment_service.consumer import PaymentConsumer
+from app.payment_service.schemas import IncomingPayment
+from config.base import logger, rabbitmq_manager
+
+
+class NotificationProducer(NotificationPubSub):
+    """Produce and publish notification messages to a RabbitMQ exchange."""
+
+    async def produce_notifications(self, payment: IncomingPayment) -> None:
+        """
+        Produce notification messages to a RabbitMQ exchange.
+
+        Parameters
+        ----------
+        payment : IncomingPayment
+            The incoming payment object containing payment details to create the
+            notification object.
+        """
+        async with await self.rabbitmq_manager.get_connection() as connection:
+            channel = await self.rabbitmq_manager.get_channel(connection=connection)
+
+            notifications_exchange = await self.declare_notification_exchange(
+                channel=channel
+            )
+
+            notification = self._get_notification(payment=payment)
+            message = Message(
+                body=notification.to_message(),
+                delivery_mode=DeliveryMode.PERSISTENT,
+            )
+
+            await notifications_exchange.publish(
+                message=message, routing_key=self._get_notification_routing_key()
+            )
+            logger.info("Notification published: %s", notification)
+
+    @staticmethod
+    def _get_notification(payment: IncomingPayment) -> Notification:
+        """
+        Create an `IncomingPayment` object based on the payment data.
+
+        Parameters
+        ----------
+        payment : IncomingPayment
+            The incoming order object containing order details.
+
+        Returns
+        -------
+        Notification
+            The `Notification` object that contains the notification type, recipient,
+            and message.
+        """
+        notification = Notification(
+            type=random.choice(get_args(NotificationType)),
+            recipient="alihezarpisheh@outlook.com",
+            message=(
+                f"Your payment status for order {payment.order_id} is: {payment.status}"
+            ),
+        )
+        return notification
+
+
+if __name__ == "__main__":
+    payment_consumer = PaymentConsumer(rabbitmq_manager=rabbitmq_manager)
+    notification_producer = NotificationProducer(rabbitmq_manager=rabbitmq_manager)
+    asyncio.run(
+        payment_consumer.consume_payments(
+            on_message_func=notification_producer.produce_notifications
+        )
+    )
